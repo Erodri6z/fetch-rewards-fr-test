f54e6e7309a8f5a6f93c0c57a908f2b869862ec6
Erodri6z <erikrodriguez2417@gmail.com>
2023-07-04 17:20:18 -0500

    add some syling for the dog cards

--- src/components/DogCards/DogCards.jsx
+++ src/components/DogCards/DogCards.jsx
@@ -1,4 +1,4 @@
-import styles from './DogCards.css'
+import '' from './DogCards.css'
 
 const DogCard = (props) => {
 
@@ -6,8 +6,8 @@ const DogCard = (props) => {
 
   return (
     <>
-    <div className="card" styles="max-width: 10rem">
-      <img src={dog.img} className="card-img-top" styles="max-width: 18rem;" alt={dog.img} />
+    <div className="card" styles="max-height: 20px;">
+      <img src={dog.img} className="card-img-top" styles={"max-height: 18px; max-width: 30px;"} alt={dog.img} />
         <div className="card-body">
         <h5 className="card-title">{dog.name}</h5>
         <p className="card-text">{dog.name} is an age {dog.age}, {dog.breed} near {dog.zip_code}</p>
