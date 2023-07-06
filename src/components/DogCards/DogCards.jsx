eb110b26c13f6e0e2632be7082bc55c107bd53dd
Erodri6z <erikrodriguez2417@gmail.com>
2023-07-06 03:55:57 -0500

    rework dog cards

--- src/components/DogCards/DogCards.jsx
+++ src/components/DogCards/DogCards.jsx
@@ -11,7 +11,9 @@ const DogCard = (props) => {
       <img src={dog.img} className="card-img-top" styles={"max-height: 18px; max-width: 30px;"} alt={dog.img} />
         <div className="card-body">
         <h5 className="card-title">{dog.name}</h5>
-        <p className="card-text">{dog.name} is an age {dog.age}, {dog.breed} near {dog.zip_code}</p>
+        <p className="card-text">Age : {dog.age} year(s) old</p>
+      <p className="card-text">Breed : {dog.breed}</p>
+      <p className="card-text">Location : {dog.zip_code}</p>
       </div>
     </div>
     </>
